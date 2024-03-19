12ad4f7b5b9379b0ffe14d8a9c51662e11c5980e
Alvaro Martinez <alvaromfdv@gmail.com>
2024-03-19 20:26:10 +0100

    added css and fetched messages

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import {
+  databases,
+  DATABASE_ID,
+  COLLECTION_ID_MESSAGES,
+} from "../../appwriteConfig";
 
 const Room = () => {
-  return (
-    <div>
-    ROOM
-    </div>
-  )
-}
+  const [messages, setMessages] = useState([]);
 
-export default Room
+  useEffect(() => {
+    getMessages();
+  }, []);
+
+  const getMessages = async () => {
+    const response = await databases.listDocuments(
+      DATABASE_ID,
+      COLLECTION_ID_MESSAGES
+    );
+    console.log("RESPONSE:", response);
+    setMessages(response.documents);
+  };
+
+  return (
+    <main className="container">
+      <div className="room--container">
+        <div>
+          {messages.map((message) => (
+            <div key={message.$id} className="messages--wrapper">
+              <div className="message-header">
+                <small className="message-timestamp">{message.$createdAt}</small>
+              </div>
+              <div className="message--body">
+                <span>{message.body}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Room;
